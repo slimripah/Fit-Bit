1558b3260173e131fb4b576fd9c5cbcf1818d741
slimripah <slimripah@gmail.com>
2026-02-26 14:02:21 +0300

    use over

--- app/src/main/java/www/minet/fitbit/MainActivity.java
+++ app/src/main/java/www/minet/fitbit/MainActivity.java
@@ -1,25 +1,17 @@
 package www.minet.fitbit;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.google.gson.Gson;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
